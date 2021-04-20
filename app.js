4f7f9ecab744acc802c1708ae844c95cdf8b09e1
Victor <victoramota@gmail.com>
2021-04-19 22:04:15 -0300

    update do express

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+//indica para o express que usaremos os dados em JSON
+app.use(express.json());
+
 let usuarios = [
     {nome: 'Victor', sobrenome: 'Athayde'},
 ];
@@ -12,6 +15,22 @@ app.get('/inicio', (request, response) => {
 
 app.get('/usuarios', (request, response) => {
     return response.json(usuarios);
+});
+
+app.post('/usuarios', (request, response) => {
+   const { nome, sobrenome } = request.body;
+
+   const novoUsuario = usuarios.push({nome, sobrenome});
+
+   return response.json({nome, sobrenome});
+});
+
+app.put("/usuarios/:nome", (request, response) => {
+    const { nome } = request.params;
+
+    //Procurar qual tem nome igual e substituir o sobrenome
+
+    const { sobrenome } = request.body;
 });
 
 app.listen(3000, () => {
